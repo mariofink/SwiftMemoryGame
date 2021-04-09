cad255d0fb395d63412fdd27a20a2c2b858fefed
Mario Fink <mario.fink@goodyear.com>
2021-04-09 20:45:23 +0200

    Initial Commit

---
+++ Memory/MemoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemoryApp.swift
+//  Memory
+//
+//  Created by Mario Fink on 09.04.21.
+//
+
+import SwiftUI
+
+@main
+struct MemoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
